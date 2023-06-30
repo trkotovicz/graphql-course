fdecb3b4eff868aed73d74b493068f6b1b50adcf
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-06-29 23:21:03 -0300

    feat: creates dataloader

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,18 +1,17 @@
-const posts = async (_, { input }, { getPosts }) => {
-  const apiFiltersInput = new URLSearchParams(input);
-  const response = await getPosts('/?' + apiFiltersInput);
-  return response.json();
-};
-
 const post = async (_obj, { id }, { getPosts }) => {
   const response = await getPosts('/' + id);
   const post = await response.json();
   return post;
 };
 
-const user = async (parent, _, { getUsers }) => {
-  const response = await getUsers('/' + parent.userId);
+const posts = async (_, { input }, { getPosts }) => {
+  const apiFiltersInput = new URLSearchParams(input);
+  const response = await getPosts('/?' + apiFiltersInput);
   return response.json();
+};
+
+const user = async (parent, _, { userDataLoader }) => {
+  return userDataLoader.load(parent.userId);
 };
 
 export const postResolvers = {
