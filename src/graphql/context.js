959beef2e288b5299be2cf421bb421a38c180d76
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-07-06 15:10:09 -0300

    feat: creates Post to User relation

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -2,10 +2,12 @@ import fetch from 'node-fetch';
 import { getUsers } from './user/utils';
 import { getPosts } from './post/utils';
 import { makeUserDataLoader } from './user/dataloaders';
+import { makePostDataLoader } from './post/dataloaders';
 
 export const context = () => {
   return {
     userDataLoader: makeUserDataLoader(getUsers(fetch)),
+    postDataLoader: makePostDataLoader(getPosts(fetch)),
     getUsers: getUsers(fetch),
     getPosts: getPosts(fetch),
   };
